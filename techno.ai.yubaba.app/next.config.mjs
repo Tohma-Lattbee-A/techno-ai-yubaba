c2837864887db28e603fda450fe32ecf60ba0715
tohma.A <tohma.a.hello.jp@gmail.com>
2024-08-07 14:45:50 +0900

    deploy setting

--- techno.ai.yubaba.app/next.config.mjs
+++ techno.ai.yubaba.app/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-}
-
-export default nextConfig;
+    basePath: '/techno.ai.yubaba',
+    assetPrefix: '/techno.ai.yubaba',
+  };
+  
+  module.exports = nextConfig;
