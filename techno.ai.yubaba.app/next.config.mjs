4aabeaf1c2b491e1fe81fd4ce3dfb3f29ac06406
tohma.A <tohma.a.hello.jp@gmail.com>
2024-08-07 14:50:42 +0900

    bb

--- techno.ai.yubaba.app/next.config.mjs
+++ techno.ai.yubaba.app/next.config.mjs
@@ -1,8 +1,8 @@
-
+// @type {import('next').NextConfig} 
 const nextConfig = {
     output: "export",
     basePath: '/techno.ai.yubaba',
     assetPrefix: '/techno.ai.yubaba',
-  };
-  
-  module.exports = nextConfig;
+};
+
+export default nextConfig;
